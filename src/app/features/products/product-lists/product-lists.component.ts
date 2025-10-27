6aeee541c8d51bc2518cf324f57ce048915c88f0
mail.nishadaboobaker <mail.nishadaboobaker@gmail.com>
2025-10-27 07:09:12 +0000

    changes in product listings

--- src/app/features/products/product-lists/product-lists.component.ts
+++ src/app/features/products/product-lists/product-lists.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../../core/services/products.service';
 
 @Component({
   selector: 'app-product-lists',
   templateUrl: './product-lists.component.html',
   styleUrls: ['./product-lists.component.css']
 })
-export class ProductListsComponent {
+export class ProductListsComponent implements OnInit {
+  products: any[] = [];
 
+  constructor(private productsService: ProductsService) { }
+
+  ngOnInit(): void {
+    this.products = this.productsService.products;
+  }
 }
