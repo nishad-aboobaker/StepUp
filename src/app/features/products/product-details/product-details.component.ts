8d301828ce189429c651592eb1c3dcc49e3d1d0f
NISHAD PP <nishuanshad@gmail.com>
2025-10-31 16:45:59 +0530

    product detail skeleton completed

--- src/app/features/products/product-details/product-details.component.ts
+++ src/app/features/products/product-details/product-details.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../../core/services/products.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: any = null;
+
+  constructor(
+    private productsService: ProductsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+
+    if (productId) {
+      this.product = this.productsService.products.find(
+        (p: any) => p.id === parseInt(productId, 10)
+      );
+      if (!this.product) {
+        console.error('Product not found');
+      }
+
+    }
+
+
+  }
 
 }
+
