910e14848d32ed89515d30f80333fd41508abb6d
NISHAD PP <nishuanshad@gmail.com>
2025-11-05 10:20:04 +0530

    `Added dashboard header and layout to order management, product management, and user management components, and implemented logout button in sidenav component.`

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css'],
 })
-export class SidenavComponent {}
+export class SidenavComponent {
+  constructor(private authService: AuthService) {}
+
+  logout() {
+    this.authService.logout();
+  }
+}
